963c577477987455911f2efe54271b94f31307a7
Imran_Nazir <72697701+Imran-Nazir-485@users.noreply.github.com>
2025-03-16 00:03:35 +0500

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -6,55 +6,85 @@ import torch
 import ast
 import numpy as np
 import re
+from sklearn.metrics.pairwise import cosine_similarity
+from langchain.embeddings import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
 import sqlite3
 import json
 import numpy as np
 import gdown
 from dotenv import load_dotenv
 import random
+import re
+from openai import OpenAI
 # Load environment variables
 load_dotenv()
+# Load Sentence Transformer model
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+# Page Layout
+# Load an open-source embedding model
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-def get_prompt(query):
-    return f"""
-    You are a **Cybersecurity Expert and Data Analyst** specializing in cybercrime trends, statistics, and threat intelligence. 
-    Your task is to provide **detailed, data-driven insights** based on the latest cybersecurity reports, government databases, 
-    and verified industry sources.
+import pickle
+import faiss
 
-    ### **Response Requirements:**
-    - Provide **accurate statistics, trends, and case studies** relevant to the user's query.
-    - Use data from **credible sources** (e.g., FBI, Europol, ENISA, Verizon DBIR, etc.).
-    - Ensure responses are **comprehensive, factual, and up-to-date**.
-    - If applicable, include **visual breakdowns** (percentages, comparisons, trends).
-    - Use clear and concise language, avoiding unnecessary jargon.
+# https://drive.google.com/file/d/1qkcNnBR9m7ivbx69HawYoe-QLO2SewYp/view?usp=sharing
+output_file = "faiss_index.bin"ng
+# Download the file
+@st.cache_data
+def download_db():
+    url = f"https://drive.google.com/uc?id=1qkcNnBR9m7ivbx69HawYoe-QLO2SewYp"
+    gdown.download(url, output_file, quiet=False)
+    return output_file
 
-    ### **User Query:**
-    {query}
+f=download_db()
 
-    ### **Example Response Format:**
-    - **Overview:** [Brief explanation of the topic]
-    - **Latest Statistics:** [Year-wise or trend-based data]
-    - **Real-World Cases:** [Relevant incidents, attack vectors, and impacts]
-    - **Mitigation Strategies:** [Best practices and industry recommendations]
+# Load FAISS index
+index = faiss.read_index(f)
+# https://drive.google.com/file/d/1wYz0VHandcxXrr3krT7ILdtFYeYTL05v/view?usp=sharing
+# Download the file
+output_file = "metadata.pkl"
+@st.cache_data
+def download_db():
+    url = f"https://drive.google.com/uc?id=1wYz0VHandcxXrr3krT7ILdtFYeYTL05v"
+    gdown.download(url, output_file, quiet=False)
+    return output_file
+    
+m=download_db()
+# Load metadata
+with open(m, "rb") as mk:
+    metadata = pickle.load(mk)
 
-    Be as **insightful, fact-based, and practical** as possible.
-    No Extra text like "here is the facts".
-    """
+
+# file_id = "1ug8pf1M1tes-CJMhS_sso372tvC4RQv8"
+# output_file = "open_ai_key.txt"
+
+# # https://docs.google.com/spreadsheets/d/1Dp6Y9ps4md393F5eRZzaZhu044k4JCmrbYDxWmQ6t2g/edit?gid=0#gid=0
+# sheet_id = '1Dp6Y9ps4md393F5eRZzaZhu044k4JCmrbYDxWmQ6t2g' # replace with your sheet's ID
+# url=f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+# df=pd.read_csv(url)
+# os.environ["OPENAI_API_KEY"] = df.keys()[0]
 
 
 
-GROQ_API_KEY=os.getenv("GROQ_API_KEY")
-from langchain_groq import ChatGroq
 
-llm = ChatGroq(
-    temperature=0,
-    model="llama-3.3-70b-versatile",
-    api_key=GROQ_API_KEY
-)
 
-st.title("Infosec Insight Bot")
+# Set the page configuration to wide mode
+st.set_page_config(page_title="My App", layout="wide")
 
-query=st.text_input("Ask Query...")
-if st.button("Ask") and query!="":
-  res=llm.invoke(get_prompt(query))
-  st.write(res.content)
+
+
+st.write("Places Recommendation")
+
+
+user_profile=st.text_area("Tell Me About Yourself")
+
+if st.button("Recommend"):
+    user_embedding = model.encode([user_profile], convert_to_numpy=True)
+
+    D, I = index.search(query_embedding, k=10)  # Get top 3 similar rows
+        
+    for idx in I[0]:
+        st.write(idx)
+
+
